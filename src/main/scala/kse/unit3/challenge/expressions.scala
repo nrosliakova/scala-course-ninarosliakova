6c6633e1fb948d3e86cbabda405def6c45014c7c
Nina Rosliakova <nrosliakova@kse.org.ua>
2026-03-21 15:47:13 +0200

    unit-3 tasks done

--- src/main/scala/kse/unit3/challenge/expressions.scala
+++ src/main/scala/kse/unit3/challenge/expressions.scala
@@ -9,8 +9,8 @@ object expressions:
     def substitute(variable: Variable, expression: Expression): Expression
 
   sealed trait Boolean extends Expression:
-    val evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
+    val evaluate: Expression                                                 = this
+    def substitute(variable: Variable, substitution: Expression): Expression = this
 
   type True = True.type
   case object True extends Boolean
@@ -19,33 +19,67 @@ object expressions:
   case object False extends Boolean
 
   case class Variable(name: String) extends Expression:
-    val evaluate: Expression                                               = ???
-    def substitute(variable: Variable, expression: Expression): Expression = ???
+    val evaluate: Expression                                               = this
+    def substitute(variable: Variable, expression: Expression): Expression =
+      if this == variable then expression
+      else this
 
   case class Negation(expression: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+    def evaluate: Expression                                                 =
+      expression.evaluate match
+        case True => False
+        case False => True
+        case Negation(exp) => exp.evaluate
+        case exp => Negation(exp)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Negation(expression.substitute(variable, substitution))
+    override def toString: String = s"!$expression"
 
   case class Conjunction(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+    def evaluate: Expression                                                 =
+      val l = left.evaluate
+      val r = right.evaluate
+      (l, r) match
+        case (False, _) => False
+        case (_, False) => False
+        case (True, x) => x
+        case (x, True) => x
+        case (x, y) => Conjunction(x, y)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Conjunction(left.substitute(variable, substitution), right.substitute(variable, substitution))
+    override def toString: String = s"$left ∧ $right"
 
   case class Disjunction(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+    def evaluate: Expression                                                 =
+      val l = left.evaluate
+      val r = right.evaluate
+      (l, r) match
+        case (True, _) => True
+        case (_, True) => True
+        case (False, x) => x
+        case (x, False) => x
+        case (x, y) => Disjunction(x, y)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Disjunction(left.substitute(variable, substitution), right.substitute(variable, substitution))
+    override def toString: String = s"$left ∨ $right"
 
   case class Implication(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+    def evaluate: Expression = Disjunction(Negation(left), right)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Implication(left.substitute(variable, substitution), right.substitute(variable, substitution))
+    override def toString: String = s"$left → $right"
 
   case class Equivalence(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+    def evaluate: Expression = Conjunction(Implication(left, right), Implication(right, left))
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Equivalence(left.substitute(variable, substitution), right.substitute(variable, substitution))
+
+    override def toString: String = s"$left ↔ $right"
 
   given Conversion[String, Variable] with
     def apply(str: String): Variable = Variable(str)
@@ -53,16 +87,16 @@ object expressions:
   extension (expr: Expression)
 
     @targetName("negation")
-    infix def unary_! : Negation = ???
+    infix def unary_! : Negation = Negation(expr)
 
     @targetName("conjunction")
-    infix def ∧(that: Expression): Conjunction = ???
+    infix def ∧(that: Expression): Conjunction = Conjunction(expr, that)
 
     @targetName("disjunction")
-    infix def ∨(that: Expression): Disjunction = ???
+    infix def ∨(that: Expression): Disjunction = Disjunction(expr, that)
 
     @targetName("implication")
-    infix def →(that: Expression): Implication = ???
+    infix def →(that: Expression): Implication = Implication(expr, that)
 
     @targetName("equivalence")
-    infix def ↔(that: Expression): Equivalence = ???
+    infix def ↔(that: Expression): Equivalence = Equivalence(expr, that)
